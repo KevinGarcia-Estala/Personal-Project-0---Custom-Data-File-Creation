80b9ad37cea062da1f6cc74176a9dea7984c1e8b
Kevin Garcia <kevingarciaestala@outlook.com>
2024-01-01 15:05:51 -0600

    Began on project 0 implemented interface

---
+++ src/Interface.java
@@ -0,0 +1,10 @@
+public interface Interface {
+    // will be used for whole positive and negative numbers
+    long basicNumbers();
+    // will be used for decimal numbers
+    double decimalNumbers();
+    // will be used for deciding what format will be used
+    void format();
+    // will write on to the data file
+    void write();
+}
---
+++ src/dataCreation.java
@@ -0,0 +1,30 @@
+
+public class dataCreation implements Interface {
+    public static void main(String[] args) {
+  
+    }
+    @Override
+    public long basicNumbers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'basicNumbers'");
+    }
+
+    @Override
+    public double decimalNumbers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'decimalNumbers'");
+    }
+
+    @Override
+    public void format() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'format'");
+    }
+
+    @Override
+    public void write() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'write'");
+    }
+    
+}
