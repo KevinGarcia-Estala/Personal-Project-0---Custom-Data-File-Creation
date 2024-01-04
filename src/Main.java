94fa87779bf644d46ba89cbb0a205b1340c9e270
Kevin Garcia <kevingarciaestala@outlook.com>
2024-01-04 16:08:30 -0600

    Main is completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+
 
 public class Main {
-    private static String temp;
     private static boolean run = true;
 
     public static void main(String[] args) {
@@ -23,25 +22,8 @@ public class Main {
     }
 
     /*
-     * ? this is just used to clear out the terminal
+     ? this is just used to clear out the terminal
      * System.out.print("\033[H\033[2J");
      * System.out.flush();
      */
-    // static void restartProgram(dataCreation newData) {
-    //     Scanner mainConsole = new Scanner(System.in);
-    //     do {
-    //         System.out.print("Would you like to create a new file? (y/n): ");
-    //         temp = mainConsole.next();
-    //     } while ((!(temp.equals("y"))) && (!(temp.equals("n"))));
-    //     mainConsole.close();
-    //     // clear out the terminal
-    //     if (temp.equals("y")) {
-    //         System.out.print("\033[H\033[2J");
-    //         System.out.flush();
-    //         newData.userInput();
-    //     } else {
-    //         System.out.println("Thank you for using this program, good bye!");
-    //         run = false;
-    //     }
-    // }
 }
